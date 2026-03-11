73231ddc5f897d0b23b5dfb6711e0a937617b40e
Leo wilis <leowilis9898@gmail.com>
2026-03-11 23:41:21 +0700

    Done

--- src/lib/axios.ts
+++ src/lib/axios.ts
@@ -1,14 +1,17 @@
 import axios from "axios";
 
+const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL || "https://be-social-media-api-production.up.railway.app/api";
+
 export const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_BASE_URL,
+  baseURL: BASE_URL,
 });
 
 api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
+  if (typeof window !== "undefined") {
+    const token = localStorage.getItem("token");
+    if (token) {
+      config.headers.Authorization = `Bearer ${token}`;
+    }
   }
 
   return config;
