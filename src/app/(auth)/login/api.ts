ac2dacf9b0d462a8a22a558c2f1499f0464a42bd
Leo wilis <leowilis9898@gmail.com>
2026-04-30 17:09:13 +0700

    refactor(auth): remove duplicate login api and type files

--- src/app/(auth)/login/api.ts
+++
@@ -1,7 +0,0 @@
-import { api } from "@/lib/axios";
-import { LoginPayload, LoginResponse } from "./type";
-
-export async function loginUser(payload: LoginPayload) {
-  const res = await api.post<LoginResponse>("/auth/login", payload);
-  return res.data;
-}
