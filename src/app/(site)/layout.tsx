4806e0ba8d6bc928b635f0dbf196d347de2cc992
Leo wilis <leowilis9898@gmail.com>
2026-07-20 05:57:51 +0700

    refactor: update site layout and related components

--- src/app/(site)/layout.tsx
+++ src/app/(site)/layout.tsx
@@ -2,54 +2,41 @@
 
 import { Suspense } from 'react';
 import { useSearchParams } from 'next/navigation';
-
+import LoadingState from '@/components/common/LoadingState';
 import Navbar from '@/components/site/header/Navbar';
 import { HomeBottomNav } from '@/components/site/bottom/BottomNav';
-import LoadingState from '@/components/common/LoadingState';
 
-interface LayoutContentProps {
+interface SearchLayoutProps {
   children: React.ReactNode;
 }
 
-function LayoutContent({ children }: LayoutContentProps) {
+function LayoutContent({ children }: SearchLayoutProps) {
   const searchParams = useSearchParams();
 
   const hasPost = searchParams.has('postId');
 
-  const contentClass = hasPost
-    ? 'pt-20 pb-32 md:pt-0 md:pb-0'
-    : 'pt-[72px] pb-32 md:pt-2 md:pb-0';
-
   return (
-    <div className='min-h-screen bg-black text-white'>
-      {/* Desktop Navbar */}
-      {!hasPost && (
-        <div className='hidden md:block'>
-          <Navbar />
-        </div>
-      )}
+    <div className='min-h-screen bg-black'>
+      {!hasPost && <Navbar />}
 
-      {/* Mobile Navbar */}
-      {!hasPost && (
-        <div className='md:hidden'>
-          <Navbar />
-        </div>
-      )}
-
-      <main className={contentClass}>{children}</main>
+      <main
+        className={
+          hasPost
+            ? 'pt-20 pb-32 md:pt-0 md:pb-0'
+            : 'pt-[72px] pb-32 md:pt-2 md:pb-0'
+        }
+      >
+        {children}
+      </main>
 
       {!hasPost && <HomeBottomNav />}
     </div>
   );
 }
 
-export default function SiteLayout({
-  children,
-}: {
-  children: React.ReactNode;
-}) {
+export default function SearchLayout({ children }: SearchLayoutProps) {
   return (
-    <Suspense fallback={<LoadingState text='Loading...' />}>
+    <Suspense fallback={<LoadingState text='Loading search...' />}>
       <LayoutContent>{children}</LayoutContent>
     </Suspense>
   );
