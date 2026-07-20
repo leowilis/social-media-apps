84a92413086e3284a70998aac1cf4f118c951a0a
Leo wilis <leowilis9898@gmail.com>
2026-07-20 21:45:13 +0700

    feat: add likeKeys moved from useLike file

--- src/hooks/post/key.ts
+++ src/hooks/post/key.ts
@@ -5,3 +5,10 @@ export const postKeys = {
   detail: (id: number) => ['posts', id] as const,
   me: ['me', 'posts'] as const,
 };
+
+export const likeKeys = {
+  post: (postId: number) => ['posts', postId] as const,
+  likes: (postId: number) => ['posts', postId, 'likes'] as const,
+  feed: ['feed'] as const,
+  myLikes: ['me', 'likes'] as const,
+};
