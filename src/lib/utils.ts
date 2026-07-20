b6fdc139ce9fbf2fe52600b531bf1668721698a8
Leo wilis <leowilis9898@gmail.com>
2026-07-20 19:10:12 +0700

    feat(utils): add followbutton class

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -4,3 +4,11 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export function getFollowButtonClass(
+  isFollowing: boolean,
+) {
+  return isFollowing
+    ? 'border border-primary-400/20 bg-primary-400/15 text-primary-400'
+    : 'border border-white/10 bg-white/5 text-white';
+}
